1e6c47330d5797e020e5009a1dd51f6e8c58dc5e
Aswexx <a284ru8ccc@gmail.com>
2023-05-05 16:06:07 +0800

    Add page loading indicator

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="min-h-[100dvh] max-w-[1200px] mx-auto flex flex-col sm:flex-row">
 		<NavBar/>
+		<NuxtLoadingIndicator color="#661AE6" :height="7"/>
 		<NuxtPage class="bg-gray-700 w-full flex-1"/>
 		<Toast/>
 	</div>
